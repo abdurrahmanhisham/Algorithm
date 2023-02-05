f7ac6ed88dc2701d8b781509e32cdc0a15a55a6d
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2023-02-05 12:23:48 +0600

    Create Function.c

---
+++ Function.c
@@ -0,0 +1,24 @@
+/*
+#include<stdio.h>
+void aaa(int n){
+    if(n%2)
+    printf("odd\n");
+    else
+    printf("even\n");
+}
+    int main(){
+        int a;
+        scanf("%d",&a);
+        aaa(a);
+}
+*/
+#include<stdio.h>
+int sum(int n){
+if(n==1) return 1;
+return n+sum(n-1);
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",sum(n));
+}
