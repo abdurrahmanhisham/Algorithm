5641b3b94491b24bc53ee5cc57ea289c0854aa56
Abdur Rahman <87389979+abdurrahmanhisham@users.noreply.github.com>
2023-02-06 13:46:10 +0600

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -81,5 +81,22 @@ int main(){
     printf("%d",GCD(x,y));
 }
 
+...................................
+    
+    #include<stdio.h>
+void reverse(int *a,int n){
+    for(int i=n-1;i>=0;i--){
+        printf("%d ",*(a+i));
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",a+i);
+    }
+    reverse(a,n);
+}
 
 
